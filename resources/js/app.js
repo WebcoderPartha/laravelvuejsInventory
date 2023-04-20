ba5e73b03284476be821302ade76de9e618803c8
WebcoderPartha <parthadev76@gmail.com>
2023-04-20 15:27:41 +0600

    User store

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,11 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import router from "./router";
+import User from "./helper/User";
+window.User = User;
 
 import Main from "./Main.vue";
+import win from "../../public/backend/plugins/uplot/uPlot.esm";
 const app = createApp(Main);
 
 
